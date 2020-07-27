bf148064253e11a2aac46dd1392a401dc4f29358
Akash Mangal <33371730+kaushlam@users.noreply.github.com>
2020-07-28 00:25:05 +0530

    Update avatar.dart

--- lib/pages/avatar.dart
+++ lib/pages/avatar.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:path/path.dart' as Path;
 
 class ProfileAvatar extends StatefulWidget {
   @override
@@ -13,10 +15,29 @@ class _ProfileAvatarState extends State<ProfileAvatar> {
   @override
   var url = 'https://bit.ly/3eWVEEt';
   File _profileImage;
+  String _uploadedFileURL;
+
+  //function for image uploading
+  Future uploadFile() async {
+    StorageReference storageReference = FirebaseStorage.instance
+        .ref()
+        .child('profiles/${Path.basename(_profileImage.path)}}');
+    StorageUploadTask uploadTask = storageReference.putFile(_profileImage);
+    await uploadTask.onComplete;
+    print('File Uploaded');
+    storageReference.getDownloadURL().then((fileURL) {
+      setState(() {
+        _uploadedFileURL = fileURL;
+      });
+    });
+  }
+
   selectImage() async {
     var imgy = await ImagePicker.pickImage(source: ImageSource.gallery);
     setState(() {
-      _profileImage = imgy; //TODO: _profileImage should be uploaded Firebase.
+      _profileImage = imgy;
+      if(_profileImage!=null)
+        uploadFile();//TODO: _profileImage should be uploaded Firebase.
     });
   }
 
@@ -26,7 +47,7 @@ class _ProfileAvatarState extends State<ProfileAvatar> {
         children: <Widget>[
           CircleAvatar(
             backgroundImage:
-                NetworkImage(url), // TODO: This method should be replace...
+            NetworkImage(url), // TODO: This method should be replace...
             // ...in order to get _profileImage from the Firebase.
             radius: 50,
             backgroundColor: Colors.white,
